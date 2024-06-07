4b9fcee2e066dc85b2f4feec9a67c2e473405854
NanMinuk <nanminuk1@gmail.com>
2024-06-08 03:27:56 +0900

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -5,9 +5,12 @@ import torch.nn.functional as F
 from transformers import BertTokenizer
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+from utils import insert_user_preferences, update_user_preferences
 from model import ArticleClassifier
 from ArticleDataset import ArticleDataset
+from predict import predict_with_classifier
 from train import train_save_personal_classifier, load_training_data
+from utils import *
 from sklearn.model_selection import train_test_split
 from torch.optim import Adam
 from torch.nn import CrossEntropyLoss
@@ -21,7 +24,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 excel_file_path = 'sum_article_extend.xlsx'
 
 # 라벨 인코더 로드 및 태그 매핑 생성
-label_mapping = {
+univ_label_mapping = {
     '웹 개발': 0,
     '모바일 앱 개발': 1,
     'UI/UX디자인': 2,
@@ -40,7 +43,7 @@ label_mapping = {
     '품질 관리': 15
 }
 
-inverse_label_mapping = {v: k for k, v in label_mapping.items()}
+univ_inverse_label_mapping = {v: k for k, v in univ_label_mapping.items()}
 
 # 모델 초기화 및 로드
 num_classes = 16
@@ -51,65 +54,31 @@ universal_classify_model.eval()
 
 tokenizer = BertTokenizer.from_pretrained("monologg/kobert")
 
-def encode_label(tag):
-    return label_mapping.get(tag, -1)  # -1은 없는 태그에 대한 디폴트 값입니다.
 
-def decode_label(label):
-    return inverse_label_mapping.get(label, 'Unknown')  # 'Unknown'은 없는 라벨에 대한 디폴트 값입니다.
+def univ_encode_label(tag):
+    return univ_label_mapping.get(tag, -1)  # -1은 없는 태그에 대한 디폴트 값.
+
+def univ_decode_label(label):
+    return univ_inverse_label_mapping.get(label, 'Unknown')  # 'Unknown'은 없는 라벨에 대한 디폴트 값.
+
+def encode_personal_label(tag, label_mapping):
+    return label_mapping.get(tag, -1)  # 없는 태그는 -1로 인코딩   
 
 # 사용자의 관심 분야에 따라 개인 분류기를 구성하는 함수
-def create_personal_classifier(user_preferences, userID):
-    train_data = load_training_data(user_preferences)
-    model = train_save_personal_classifier(train_data,tokenizer, user_preferences, userID)
+def create_personal_classifier(userID,user_preferences):
+    summarized_text, labels = load_training_data(userID)
+    model = train_save_personal_classifier(summarized_text, labels,tokenizer, user_preferences, userID)
     # TODO
     # training_data를 사용하여 개인 분류기를 학습 코드 작성
-    return
-
-# 분류 예측  함수
-import torch
-import torch.nn.functional as F
-
-def predict_with_classifier(text, model, tokenizer):
-    encoding = tokenizer.encode_plus(
-        text,
-        add_special_tokens=True,
-        max_length=512,
-        padding='max_length',
-        return_attention_mask=True,
-        return_tensors='pt',
-        truncation=True
-    )
-    input_ids = encoding['input_ids']
-    attention_mask = encoding['attention_mask']
-
-    with torch.no_grad():
-        input_ids = input_ids.to('cpu')
-        attention_mask = attention_mask.to('cpu')
-        outputs = model(input_ids, attention_mask)
-
-        # 모델의 출력이 logits일 경우 그대로 사용
-        if isinstance(outputs, tuple):
-            logits = outputs[0]
-        else:
-            logits = outputs
-
-        # 소프트맥스 함수로 확률 계산
-        probabilities = F.softmax(logits, dim=1)
-        
-        # 모든 클래스의 확률
-        all_probs = probabilities.cpu().numpy().flatten().tolist()
-        all_classes = list(range(len(all_probs)))
-        
-        # 가장 높은 확률을 가진 클래스
-        predicted_class = all_classes[all_probs.index(max(all_probs))]
-
-    return predicted_class, all_classes, all_probs
+    return 
 
 # personal_classifier 가져오기
-def get_personal_classifier(userID,num_classes):
+def get_personal_classifier(userID):
     # TODO
     # DB와 연결해 personal classifier 가져오기 구현
     model_save_path = f'{userID}.pth'
+    user_preferences, _ = get_user_preferences(userID)
+    num_classes = len(user_preferences)
     personal_classify_model = ArticleClassifier(num_classes=num_classes)
     personal_classify_model.load_state_dict(torch.load(model_save_path, map_location=torch.device('cpu')))
     personal_classify_model.eval()
@@ -121,20 +90,21 @@ def get_personal_classifier(userID,num_classes):
 def create_personal_classifier_endpoint():
     data = request.get_json()
     userID = data['userID']
+    print(userID)
     user_preferences = data['user_preferences']
+    print(user_preferences)
+    insert_user_preferences(userID,user_preferences)
     create_personal_classifier(userID, user_preferences)
-    return jsonify({'message': 'Personal classifier built successfully.'})
+    return jsonify({'message': '개인 분류기 생성 완료. user123.pth파일 생성된것 확인'})
 
 # 사용자의 관심 분야에 따라 개인 분류기 예측 엔드포인트
 @app.route('/classify_with_personal_classifier', methods=['POST'])
 def predict_with_personal_classifier_endpoint():
     data = request.get_json()
     # TODO
-    # 개인변 상태 관리
+    # 개인별 상태 관리
     userID = data['userID']
     text = data['summarized_text']
-
-    
     #personal_label_mapping = {0: 3, 1: 5, 2: 8}
     #inverse_personal_label_mapping = {v: k for k, v in personal_label_mapping.items()}
 
@@ -151,9 +121,9 @@ def predict_with_personal_classifier_endpoint():
         else:
             break  # 차이가 20% 이상이면 종료
 
-    predicted_class = decode_label(predicted_class)
-    top_tags = [inverse_label_mapping[class_idx] for class_idx in top_classes]
-    possible_tags = [inverse_label_mapping[class_idx] for class_idx in possible_classes]
+    predicted_class = univ_decode_label(predicted_class)
+    top_tags = [univ_inverse_label_mapping[class_idx] for class_idx in top_classes]
+    possible_tags = [univ_inverse_label_mapping[class_idx] for class_idx in possible_classes]
     return jsonify({
         'predicted_class': predicted_class,
         'possible_classes': possible_tags
@@ -176,9 +146,9 @@ def predict_with_universal_classifier_endpoint():
         else:
             break  # 차이가 20% 이상이면 종료
     
-    predicted_class = decode_label(predicted_class)
-    top_tags = [inverse_label_mapping[class_idx] for class_idx in top_classes]
-    possible_tags = [inverse_label_mapping[class_idx] for class_idx in possible_classes]
+    predicted_class = univ_decode_label(predicted_class)
+    top_tags = [univ_inverse_label_mapping[class_idx] for class_idx in top_classes]
+    possible_tags = [univ_inverse_label_mapping[class_idx] for class_idx in possible_classes]
     print(possible_tags)
     
     return jsonify({
@@ -186,16 +156,38 @@ def predict_with_universal_classifier_endpoint():
         'possible_classes': possible_tags
     })
 
-
-
-# 사용자의 관심 분야를 업데이트하고 개인 분류기를 업데이트하는 엔드포인트
+# 사용자 classifier 업데이트하는 엔드포인트
 @app.route('/update_personal_classifier', methods=['POST'])
+def update_personal_classfier_endpoint():
+    data = request.get_json()
+    userID = data['userID']
+    summarized_text = data['summarized_text']
+    label = data['user_update_label']
+    
+
+    user_preference,label_mapping = get_user_preferences(userID)
+    org_summarized_text, org_labels = load_training_data(userID)
+    
+
+    new_summarized_text = pd.Series([summarized_text])
+    new_labels = pd.Series([label])
+    new_labels = new_labels.apply(lambda x: encode_personal_label(x, label_mapping))
+
+    merged_summarized_text = pd.concat([org_summarized_text, new_summarized_text], ignore_index=True)
+    merged_labels = pd.concat([org_labels, new_labels], ignore_index=True)
+    train_save_personal_classifier(merged_summarized_text,merged_labels,tokenizer,user_preference,userID)
+
+
+    return jsonify({'message': '개인 분류기 업데이트됨. user123.pth(최신파일), user123.pth_2024XXXX (예전 파일)'})
+
+# 사용자의 관심 분야를 업데이트
+@app.route('/update_user_preference', methods=['POST'])
 def update_user_preferences_endpoint():
     data = request.get_json()
     userID = data['userID']
     user_preferences = data['user_preferences']
     update_user_preferences(userID, user_preferences)
-    return jsonify({'message': 'User preferences updated successfully.'})
+    return jsonify({'message': '관심사 업데이트 됨. DB를 확인해주세요'})
 
 if __name__ == '__main__':
     
