e256d212ee468509905e950913bf8877d7c39b61
NanMinuk <nanminuk1@gmail.com>
2024-06-13 01:07:50 +0900

    Update model.py

--- flask/model.py
+++ flask/model.py
@@ -4,14 +4,18 @@ import torch.nn as nn
 from transformers import BertModel
 
 class ArticleClassifier(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, dropout_rate=0.3):
         super(ArticleClassifier, self).__init__()
-        self.bert = BertModel.from_pretrained("monologg/kobert")
+        self.bert = BertModel.from_pretrained("klue/bert-base")
         hidden_size = self.bert.config.hidden_size
+
+        self.dropout = nn.Dropout(dropout_rate)
         self.classifier = nn.Linear(hidden_size, num_classes)
 
     def forward(self, input_ids, attention_mask):
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         sequence_output = outputs.pooler_output
+        sequence_output = self.dropout(sequence_output)
         logits = self.classifier(sequence_output)
-        return logits
+        probs = torch.sigmoid(logits)
+        return probs
